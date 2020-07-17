0eb732907a81727a45b9e0d5b80b10dc2da56556
mahbub095 <mahbub.095@gmail.com>
2020-07-18 02:51:49 +0600

    Try to fetch image

--- resources/views/productsdetails.blade.php
+++ resources/views/productsdetails.blade.php
@@ -568,23 +568,23 @@
                     <div class="col-md-6">
                         <div id="product-main-view">
                             <div class="product-view">
+                                <img src="{{$pro->feature_image}}" alt="">
+                            </div>
+                           {{-- <div class="product-view">
+                                <img src="{{$pro->feature_image}}" alt="">
+                            </div>--}}
+                            {{--<div class="product-view">
                                 <img src="<?php echo $pro->feature_image; ?>" alt="">
                             </div>
-                       {{--     <div class="product-view">
-                                <img src="{{asset('front/img/main-product02.jpg')}}" alt="">
-                            </div>
                             <div class="product-view">
-                                <img src="{{asset('front/img/main-product03.jpg')}}" alt="">
-                            </div>
-                            <div class="product-view">
-                                <img src="{{asset('front/img/main-product04.jpg')}}" alt="">
+                                <img src="<?php echo $pro->feature_image; ?>" alt="">
                             </div>--}}
                         </div>
                         <div id="product-view">
                             <div class="product-view">
-                                <img src="{{asset('front/img/thumb-product01.jpg')}}" alt="">
+                                <img src="{{--{{$pro->feature_image}}--}}" alt="">
                             </div>
-                            <div class="product-view">
+                           {{-- <div class="product-view">
                                 <img src="{{asset('front/img/thumb-product02.jpg')}}" alt="">
                             </div>
                             <div class="product-view">
@@ -592,7 +592,7 @@
                             </div>
                             <div class="product-view">
                                 <img src="{{asset('front/img/thumb-product04.jpg')}}" alt="">
-                            </div>
+                            </div>--}}
                         </div>
                     </div>
                         <?php } ?>
@@ -766,7 +766,7 @@
                             </div>
                         </div>
                     </div>
-                    <?php } ?>
+                        <?php } ?>
                 </div>
                 <!-- /Product Details -->
             </div>
@@ -789,13 +789,15 @@
                     </div>
                 </div>
                 <!-- section title -->
+                <?php
 
-                <!-- Product Single -->
+                foreach($product as $pro){
+                    ?>
                 <div class="col-md-3 col-sm-6 col-xs-6">
                     <div class="product product-single">
                         <div class="product-thumb">
                             <button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Quick view</button>
-                            <img src="./img/product04.jpg" alt="">
+                            <img src="{{$pro->feature_image}}" alt="">
                         </div>
                         <div class="product-body">
                             <h3 class="product-price">$32.50</h3>
@@ -816,96 +818,8 @@
                     </div>
                 </div>
                 <!-- /Product Single -->
+            <?php } ?>
 
-                <!-- Product Single -->
-                <div class="col-md-3 col-sm-6 col-xs-6">
-                    <div class="product product-single">
-                        <div class="product-thumb">
-                            <div class="product-label">
-                                <span>New</span>
-                            </div>
-                            <button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Quick view</button>
-                            <img src="./img/product03.jpg" alt="">
-                        </div>
-                        <div class="product-body">
-                            <h3 class="product-price">$32.50</h3>
-                            <div class="product-rating">
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star-o empty"></i>
-                            </div>
-                            <h2 class="product-name"><a href="#">Product Name Goes Here</a></h2>
-                            <div class="product-btns">
-                                <button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
-                                <button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>
-                                <button class="primary-btn add-to-cart"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- /Product Single -->
-
-                <!-- Product Single -->
-                <div class="col-md-3 col-sm-6 col-xs-6">
-                    <div class="product product-single">
-                        <div class="product-thumb">
-                            <div class="product-label">
-                                <span class="sale">-20%</span>
-                            </div>
-                            <button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Quick view</button>
-                            <img src="./img/product02.jpg" alt="">
-                        </div>
-                        <div class="product-body">
-                            <h3 class="product-price">$32.50 <del class="product-old-price">$45.00</del></h3>
-                            <div class="product-rating">
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star-o empty"></i>
-                            </div>
-                            <h2 class="product-name"><a href="#">Product Name Goes Here</a></h2>
-                            <div class="product-btns">
-                                <button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
-                                <button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>
-                                <button class="primary-btn add-to-cart"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- /Product Single -->
-
-                <!-- Product Single -->
-                <div class="col-md-3 col-sm-6 col-xs-6">
-                    <div class="product product-single">
-                        <div class="product-thumb">
-                            <div class="product-label">
-                                <span>New</span>
-                                <span class="sale">-20%</span>
-                            </div>
-                            <button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Quick view</button>
-                            <img src="./img/product01.jpg" alt="">
-                        </div>
-                        <div class="product-body">
-                            <h3 class="product-price">$32.50 <del class="product-old-price">$45.00</del></h3>
-                            <div class="product-rating">
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star"></i>
-                                <i class="fa fa-star-o empty"></i>
-                            </div>
-                            <h2 class="product-name"><a href="#">Product Name Goes Here</a></h2>
-                            <div class="product-btns">
-                                <button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
-                                <button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>
-                                <button class="primary-btn add-to-cart"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 <!-- /Product Single -->
             </div>
             <!-- /row -->
